9ff5835813d1ad54158b822aa23b6b88b47a0144
Velecky <ladislav.velecky@tieto.com>
2016-10-04 10:24:34 +0200

    Installed roles on DCs

--- RolesonDCs.ps1
+++ RolesonDCs.ps1
@@ -1,3 +1,3 @@
-$Servers= Get-Content C:\Users\Administrator\DCS.txt;
+$Servers= Get-Content C:\Users\Administrator\DCS.txt
 Write-Output $Servers
 Get-WindowsFeature -ComputerName $Servers | Where Installed | Out-File -FilePath test.csv
